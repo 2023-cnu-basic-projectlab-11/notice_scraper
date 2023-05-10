164846aec21cce4197b1eae4b8fe8bbd996f4378
Kim3339 <jooho5580@gmail.com>
2023-05-10 22:59:44 +0900

    notice.dart 주석 추가

--- lib/notice.dart
+++ lib/notice.dart
@@ -2,10 +2,16 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'notice.g.dart';
 
+/// 공지 모델 클래스
 @JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
 class Notice {
+  /// 공지 제목
   final String title;
+
+  /// 공지가 올라간 날짜
   final DateTime datetime;
+
+  /// 공지가 올라간 사이트
   final Origin origin;
 
   const Notice(this.title, this.datetime, this.origin);
@@ -15,11 +21,19 @@ class Notice {
   Map<String, dynamic> toJson() => _$NoticeToJson(this);
 }
 
+/// 공지 원본 사이트 정보를 담고 있는 모델 클래스
 @JsonSerializable(fieldRename: FieldRename.snake)
 class Origin {
+  /// 식별자. 알파벳과 언더바(_)로 이루어져있다.
   final String id;
+
+  /// 사이트 이름
   final String name;
+
+  /// 사이트 설명
   final String description;
+
+  /// 사이트 기본 base uri
   final String baseUri;
 
   const Origin(this.id, this.name, this.description, this.baseUri);
