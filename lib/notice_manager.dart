51c72b2aedb0fa6a9ff17bedf27520103bc90e34
Kim3339 <jooho5580@gmail.com>
2023-05-15 02:29:46 +0900

    임시 주석 삭제

--- lib/notice_manager.dart
+++ lib/notice_manager.dart
@@ -34,16 +34,3 @@ class NoticeManager {
   DateTime? getLastUpdated(Origin origin) =>
       DateTime.tryParse(_storage.getItem(origin.endpointUrl) ?? "");
 }
-
-
-
-/*
-[
-  Notice('공지1', DateTime.now().subtract(const Duration(days: 1)),
-      const Origin('테스트 사이트', '테스트용 사이트입니다', 'example1.com')),
-  Notice('공지2', DateTime.now().subtract(const Duration(days: 2)),
-      const Origin('테스트 사이트', '테스트용 사이트입니다', 'example2.com')),
-  Notice('공지3', DateTime.now().subtract(const Duration(days: 3)),
-      const Origin('테스트 사이트', '테스트용 사이트입니다', 'example3.com'))
-]
-*/
